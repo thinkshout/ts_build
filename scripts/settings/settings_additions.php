6b3358d65e46b0c4220c0fea494e2c6c2c8072ef
Alex Ellison <alex.ellison@thinkshout.com>
2015-10-22 11:13:03 -0700

    minor space tweaks to settings_additions.php

--- scripts/settings/settings_additions.php
+++ scripts/settings/settings_additions.php
@@ -1,9 +1,10 @@
+
 // Site configuration settings.
 if (file_exists(DRUPAL_ROOT . '/' . conf_path() . '/site.settings.php')) {
-include_once DRUPAL_ROOT . '/' . conf_path() . '/site.settings.php';
+  include_once DRUPAL_ROOT . '/' . conf_path() . '/site.settings.php';
 }
 
 // Local site configuration settings.
 if (file_exists(DRUPAL_ROOT . '/' . conf_path() . '/local.settings.php')) {
-include_once DRUPAL_ROOT . '/' . conf_path() . '/local.settings.php';
+  include_once DRUPAL_ROOT . '/' . conf_path() . '/local.settings.php';
 }
