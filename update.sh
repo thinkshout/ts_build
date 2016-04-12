cf370f37d828d9b94f6a67f1e20682b372355197
Mike McCaffrey <mike.mccaffrey@thinkshout.com>
2016-04-12 13:28:25 -0700

    Updating update.sh to rename old core.make and site.make files and to create new site.settings.php and local.settings.php files if needed

--- update.sh
+++ update.sh
@@ -13,6 +13,26 @@ if [ -f settings/settings.php ]; then
   mv settings/settings.php settings/site.settings.php
 fi
 
+if [ ! -f settings/site.settings.php ]; then
+  echo "Creating new site.settings.php file"
+  printf "<?php \n\n/**\n * @file site.settings.php\n * Add site-wide settings here, which will be included from settings.php\n */\n\n" > settings/site.settings.php
+fi
+
+if [ ! -f settings/local.settings.php ]; then
+  echo "Creating new local.settings.php file"
+  printf "<?php \n\n/**\n * @file local.settings.php\n * Add settings for your local environment here, which will be included from settings.php\n */\n\n" > settings/local.settings.php
+fi
+
+if [ -f core.make ]; then
+  echo "Moving core.make to drupal-org-core.make"
+  mv core.make drupal-org-core.make
+fi
+
+if [ -f site.make ]; then
+  echo "Moving site.make to drupal-org.make"
+  mv site.make drupal-org.make
+fi
+
 echo "Getting new settings_additions.php"
 [ ! -f "settings/settings_additions.php" ] && curl -fsSL https://raw.githubusercontent.com/thinkshout/ts_build/master/settings/settings_additions.php > settings/settings_additions.php
 echo "Getting new .gitignore"
