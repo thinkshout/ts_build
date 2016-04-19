4dc7dbbec261087d77c82d754e7c6d2147157544
Mike McCaffrey <mike.mccaffrey@thinkshout.com>
2016-04-19 14:53:11 -0700

    Add more defaults to local.settings.php

--- settings/local.settings.php
+++ settings/local.settings.php
@@ -3,6 +3,7 @@
 /**
  * Specify the database settings for your local environment
  */
+
 # $databases = array (
 #   'default' =>
 #   array (
@@ -22,4 +23,18 @@
 /**
  * Specify the base_url so the site works with local virtualhosts
  */
+
 # $base_url = 'http://project-name.dev';
+
+/**
+ * Specify the settings for stage file proxy so you just need to drush en the module
+ */
+
+#$conf['stage_file_proxy_hotlink'] = 1;
+#$conf['stage_file_proxy_origin'] = 'http://organization.org';
+
+/**
+ * Simple way to get the pantheon solr drupal server pointed to localhost
+ */
+
+# $conf['pantheon_apachesolr_search_api_solr_service_class'] = 'SearchApiSolrService';
